64e5eb806bf607d2b5985cbcfc24dc05a11178ba
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-20 16:45:26 +0530

    Create file

--- files.js
+++ files.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 // reading files(Async) - it has two arguments
 //first - location of file to read
 //second - callback function after task done.
+/*
 fs.readFile("./docs/blog1.txt", (err, data) => {
   if (err) {
     console.log(err);
@@ -13,3 +14,14 @@ fs.readFile("./docs/blog1.txt", (err, data) => {
 });
 
 console.log("last line");
+*/
+
+//Writing files
+fs.writeFile("./docs/blog1.txt", "hello, stars", () => {
+  console.log("written");
+});
+
+//creating and written the text inside it
+fs.writeFile("./docs/blog2.txt", "hello, ninjas", () => {
+  console.log("written again");
+});
