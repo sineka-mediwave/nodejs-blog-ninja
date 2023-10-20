20846c4060bf2d0049471f6aa7f1c420ce4caaf6
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-20 16:26:01 +0530

    modules

---
+++ modules.js
@@ -0,0 +1,8 @@
+// it gets data from the module people
+// const data = require("./people");
+const { people, ages } = require("./people");
+
+console.log(people, ages);
+
+const os = require("os");
+console.log(os.platform);
